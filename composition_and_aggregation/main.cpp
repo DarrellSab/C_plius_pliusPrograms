64e138f7f428e0337769a102e4b9d47fcab44f60
DarrellSab <darrell74@gmail.com>
2019-10-26 20:42:59 +0300

    Created example of composition and aggregation

---
+++ composition_and_aggregation/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//This class for example of aggregation
+//this class is not realated with class CUniversity
+class CProfesor
+{
+public:
+    string getName()
+    {
+        return name;
+    }
+private:
+    string name = "Jim Beam";
+};
+class CColege
+{
+public:
+    void checkJobOfProfesor()
+    {
+        cout<<"Professor "<<profesor.getName()<<" will stay at colege next friday."<<endl;
+    }
+private:
+    CProfesor profesor;
+};
+class CUniversity
+{
+public:
+    void teachingStudents()
+    {
+        student.teachingStudents();
+    }
+    void checkJobOfProfesor()
+    {
+        cout<<"This evening professor "<<profesor.getName()<<" will stay at university."<<endl;
+    }
+
+private:
+    //This class is eaxample of composition
+    class CDepartament
+    {
+    public:
+        void teachingStudents()
+        {
+            cout<<"Departaments organize teaching of students"<<endl;
+        }
+    };
+    CDepartament student;
+    CProfesor profesor;
+};
+int main() {
+    CUniversity department;
+    department.teachingStudents();
+    department.checkJobOfProfesor();
+    CColege colege;
+    colege.checkJobOfProfesor();
+
+    return 0;
+}
